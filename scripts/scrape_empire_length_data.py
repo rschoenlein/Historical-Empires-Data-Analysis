87aa2dd89c475d0aba594367d757577c26842ab7
Ryan <ryanschoenlein@gmail.com>
2023-11-19 19:19:24 -0600

    scraping logic

--- scripts/scrape_empire_length_data.py
+++ scripts/scrape_empire_length_data.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-
+from selenium.webdriver.firefox.options import Options
 
 class Scraper:
     def __init__(self, url):
@@ -8,14 +8,32 @@ class Scraper:
 
     def get_empire_tables(self):
 
-        driver = webdriver.Firefox()
+
+        options = Options()
+        options.headless = True
+
+        driver = webdriver.Firefox(options=options)
 
         driver.get(self.url)
         page_source = driver.page_source
 
         soup = BeautifulSoup(page_source, 'lxml')
 
-        print(soup)
+
+        empire_lengths = {}
+        length_elems = soup.find_all('span', attrs={'class': 'value-bullet'})
+        name_elems = soup.find_all('span', attrs={'class': 'title-bullet'})
+
+
+
+        print(length_elems)
+
+        for n, l in zip(name_elems, length_elems):
+            n_text = n.get_text()
+            l_text = l.get_text()
+            empire_lengths[n_text] = l_text
+
+        print(empire_lengths)
 
 if __name__ == '__main__':
     url = "https://247wallst.com/special-report/2021/12/25/the-longest-lived-empires-in-history/"
